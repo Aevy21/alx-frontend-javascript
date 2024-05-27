fa1e7e101ed3bad9b1fd0c4eb2e0549009dcfaeb
Avumile <Smithavery435@gmail.com>
2024-05-27 20:34:52 +0000

    using methods iterators and for...of loopd

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,12 @@
+export default function createReportObject(employeesList) {
+  const getNumberOfDepartments = (employeesList) => {
+    return Object.keys(employeesList).length;
+  };
+
+  return {
+    allEmployees: {
+      ...employeesList
+    },
+    getNumberOfDepartments
+  };
+}

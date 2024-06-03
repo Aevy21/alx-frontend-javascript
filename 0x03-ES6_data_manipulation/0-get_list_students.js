617461c9d5bb9158dd6f2c6a80e40df1e5331096
Avumile <Smithavery435@gmail.com>
2024-06-03 23:54:56 +0000

    strugglint with eslint

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,12 +2,12 @@ export default function getListStudents() {
   const studentData = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-  
-  return studentData.map(data => ({
+
+  return studentData.map((data) => ({
     id: data.id,
     firstName: data.firstName,
-    location: data.location
+    location: data.location,
   }));
 }

22aae4c3ac0137cd3d21a9189947820b043b04d0
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-06-05 22:41:14 +0200

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -20,7 +20,7 @@ interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
 
-// Step 2, 6: Define interface for the printTeacher function
+// interface for the printTeacher function
 interface TeacherInterface {
     firstName: string;
     lastName: string;
@@ -30,12 +30,12 @@ interface TeacherInterface {
     [key: string]: any;
 }
 
-// Step 4: Define Directors interface extending Teacher
+// Directors interface extending Teacher
 interface Directors extends Teacher {
     numberOfReports: number;
 }
 
-// Step 7-8: Define StudentClass class and interface
+// StudentClass class and interface
 interface StudentClassConstructor {
     new(firstName: string, lastName: string): StudentClass;
 }

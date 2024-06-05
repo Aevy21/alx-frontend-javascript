5ff41a5bee444591837b62fd4fcffffa4538bf76
Avumile <Smithavery435@gmail.com>
2024-06-05 20:30:06 +0000

    exploring typescript concepts

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,10 @@
+ole.log(employee1 instanceof Teacher); // Output: true
+console.log(employee2 instanceof Director); // Output: true
+console.log(employee3 instanceof Director); // Output: true
+
+console.log(employee1.workFromHome()); // Output: Cannot work from home
+console.log(employee2.workFromHome()); // Output: Working from home
+console.log(employee3.workFromHome()); // Output: Working from home
+
+
+

30f494dcc8ecb16b1ac20fbcee3de7035fc3e543
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-06-05 22:40:14 +0200

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,10 +1,66 @@
-ole.log(employee1 instanceof Teacher); // Output: true
-console.log(employee2 instanceof Director); // Output: true
-console.log(employee3 instanceof Director); // Output: true
+class Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean = true;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
 
-console.log(employee1.workFromHome()); // Output: Cannot work from home
-console.log(employee2.workFromHome()); // Output: Working from home
-console.log(employee3.workFromHome()); // Output: Working from home
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
 
+function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName.charAt(0)}. ${lastName}`;
+}
 
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
 
+// Step 2, 6: Define interface for the printTeacher function
+interface TeacherInterface {
+    firstName: string;
+    lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+}
+
+// Step 4: Define Directors interface extending Teacher
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+// Step 7-8: Define StudentClass class and interface
+interface StudentClassConstructor {
+    new(firstName: string, lastName: string): StudentClass;
+}
+
+interface StudentClass {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClassImpl implements StudentClass {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+const studentClassConstructor: StudentClassConstructor = StudentClassImpl;
